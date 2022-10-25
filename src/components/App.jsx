defced9caebc24995a81398989f472ea4a44dd36
LutsenkoVitaliy <lutsenko996@gmail.com>
2022-10-25 23:23:44 +0300

    addContactinLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,21 @@ class App extends Component {
     filter: ''
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+   
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
   deletedContact = contactId => {
     this.setState(prevState => ({
      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
